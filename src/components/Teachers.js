26d0316b090e39e0fde08702c5d76ac9952bfbc2
Adam Bolton <adam_bolton90@hotmail.com>
2021-11-23 23:34:39 +0900

    added text to Teachers hero section and corrected screen responsiveness

--- src/components/Teachers.js
+++ src/components/Teachers.js
@@ -4,8 +4,9 @@ import TeacherCards from "./TeacherCards";
 const Teachers = () => {
     return ( 
         <div className="teachers">
-            <h2>Our teachers are located globally. </h2>
-            <img src={teachers} alt="teachers" />
+            <h2>Our teachers are located globally </h2>
+            <img className="teachers-hero-img" src={teachers} alt="teachers" />
+            <h2>Meet some of our highly rated instructors</h2>
             <TeacherCards />
         </div>
      );
