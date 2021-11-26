0c19433644673704916d1e577afa3154be71d6f3
Adam Bolton <adam_bolton90@hotmail.com>
2021-11-26 23:28:39 +0900

    added MaterialsHero image responsiveness

--- src/components/Materials.js
+++ src/components/Materials.js
@@ -4,7 +4,7 @@ const Materials = () => {
     return ( 
         <div className="materials">
             <h1>This is the materials page</h1>
-            <h2>Start your lesson with daily conversation or news discussion</h2>
+            {/* <h2>Start your lesson with daily conversation or news discussion</h2> */}
             <MaterialsHero />
         </div>
      );
--- src/components/MaterialsHero.js
+++ src/components/MaterialsHero.js
@@ -4,8 +4,8 @@ import news from "../img/news-app-screenshot.PNG";
 const MaterialsHero = () => {
     return ( 
         <div className="materials-hero">
-            <img src={conversation} alt="a collection of conversation topics"></img>
-            <img src={news} alt="" />
+            <img id="conversation-img" src={conversation} alt="a collection of conversation topics"></img>
+            <img id="news-img" src={news} alt="a page with news articles" />
         </div>
      );
 }
