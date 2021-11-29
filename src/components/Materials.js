3aa814d8ecbd42afd10ca560e321a8250bf31a8f
Adam Bolton <adam_bolton90@hotmail.com>
2021-11-29 23:27:09 +0900

    added info card and button to conversation MaterialsHero img

--- src/components/Materials.js
+++ src/components/Materials.js
@@ -3,7 +3,7 @@ import MaterialsHero from "./MaterialsHero";
 const Materials = () => {
     return ( 
         <div className="materials">
-            <h1>This is the materials page</h1>
+            {/* <h1>This is the materials page</h1> */}
             {/* <h2>Start your lesson with daily conversation or news discussion</h2> */}
             <MaterialsHero />
         </div>
--- src/components/MaterialsHero.js
+++ src/components/MaterialsHero.js
@@ -4,7 +4,11 @@ import news from "../img/news-app-screenshot.PNG";
 const MaterialsHero = () => {
     return ( 
         <div className="materials-hero">
-            <img id="conversation-img" src={conversation} alt="a collection of conversation topics"></img>
+            <img src={conversation} alt="a collection of conversation topics"></img>
+            <div className="conversation-info">
+                <button id="conversation-btn">Jump into daily conversation</button>
+            </div>
+            
             <img id="news-img" src={news} alt="a page with news articles" />
         </div>
      );
