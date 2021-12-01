a9faa21acae372a42e06f4a6b3d10a2d6f94f568
Adam Bolton <adam_bolton90@hotmail.com>
2021-12-01 23:36:08 +0900

    added text content to conversation and news info sections and styled the background images

--- src/components/MaterialsHero.js
+++ src/components/MaterialsHero.js
@@ -6,9 +6,11 @@ const MaterialsHero = () => {
         <div className="materials-hero">
             <img src={conversation} alt="a collection of conversation topics"></img>
             <div className="conversation-info">
+                <p>Ease into lessons naturally using this material to engage in daily conversation. Select from a wide variety of 100 commonly talked-about topics.</p>
                 <button id="conversation-btn" className="materials-hero-info-btn">Jump into daily conversation</button>
             </div>
             <div className="news-info">
+                <p>Explore a range of news categories and read articles from a selection of countries. Enjoy discussion on the latest stories.</p>
                 <button id="news-btn" className="materials-hero-info-btn">Read the latest news</button>
             </div>
             <img id="news-img" src={news} alt="a page with news articles" />
