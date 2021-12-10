098054952c262cbed5417f6ce9701d06f7afd805
Adam Bolton <adam_bolton90@hotmail.com>
2021-12-10 23:06:31 +0900

    added responsive styling to pricing header

--- src/components/Pricing.js
+++ src/components/Pricing.js
@@ -1,7 +1,7 @@
 const Pricing = () => {
     return ( 
         <div className="pricing">
-            <h1 className="pricing-header">Select a suitable plan for your liking and be speaking with a teacher in a matter of minutes</h1>
+            <h1 className="pricing-header">Select a suitable plan and be speaking with a teacher in a matter of minutes</h1>
             <div className="pricing-hero">
                 Lorem ipsum dolor sit amet, consectetur adipisicing elit. Repellendus incidunt expedita cumque praesentium alias veritatis ut quia ipsa sequi? Sequi!
             </div>
