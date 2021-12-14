b61f69a8d1c47ac0e28282cd7db4a0e906ba9d5c
Adam Bolton <adam_bolton90@hotmail.com>
2021-12-14 23:38:20 +0900

    added template text and base styling to Pricing Hero

--- src/components/Pricing.js
+++ src/components/Pricing.js
@@ -9,10 +9,33 @@ const Pricing = () => {
         <div className="pricing">
             <h1 className="pricing-header">Select a suitable plan and be speaking with a teacher in a matter of minutes</h1>
             <div className="pricing-hero">
-                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Repellendus incidunt expedita cumque praesentium alias veritatis ut quia ipsa sequi? Sequi!
-                <img src={starter} alt="" />
-                <img src={regular} alt="" />
-                <img src={prime} alt="" />
+                <div className="pricing-starter">
+                    <h2>Starter</h2>
+                    <img src={starter} alt="" />
+                    <ul>
+                        <li>Lorem, ipsum dolor sit amet.</li>
+                        <li>consectetur adipisicing elit.</li>
+                        <li>Expedita blanditiis odit dolorem</li>
+                    </ul>
+                </div>
+                <div className="pricing-starter">
+                    <h2>Regular</h2>
+                    <img src={regular} alt="" />
+                    <ul>
+                        <li>laudantium omnis sapiente</li>
+                        <li>eveniet delectus unde voluptatibus sunt</li>
+                        <li>natus eaque consequuntur praesentium</li>
+                    </ul>
+                </div>
+                <div className="pricing-starter">
+                    <h2>Prime</h2>
+                    <img src={prime} alt="" />
+                    <ul>
+                        <li>voluptates voluptas numquam sint</li>
+                        <li>quam at vel maxime accusantium laborum</li>
+                        <li>Laboriosam similique velit</li>
+                    </ul>
+                </div>
             </div>
         </div>
      );
