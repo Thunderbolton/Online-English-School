85f1934443939566c1278c541ea24770209d1bb6
Adam Bolton <adam_bolton90@hotmail.com>
2021-12-21 18:27:17 +0900

    added image and screen responsiveness to Pricing Feedback

--- src/components/PricingFeedback.js
+++ src/components/PricingFeedback.js
@@ -1,7 +1,12 @@
+import feedback from "../img/feedback.svg"
+
 const PricingFeedback = () => {
     return ( 
-        <div>
-            <p>Pricing Feedback</p>
+        <div className="pricing-feedback">
+            <h2>What our students are saying</h2>
+            <div className="pricing-feedback-container">
+                <img src={feedback} alt="a woman adjusting a ratings slider" />
+            </div>
         </div>
      );
 }
