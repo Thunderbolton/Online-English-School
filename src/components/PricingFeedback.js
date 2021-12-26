e5b1afecd03e8d2cf11aa301e090c6866ab65900
Adam Bolton <adam_bolton90@hotmail.com>
2021-12-26 15:03:28 +0900

    added content to Pricing Feedback quotes

--- src/components/PricingFeedback.js
+++ src/components/PricingFeedback.js
@@ -5,10 +5,10 @@ const PricingFeedback = () => {
     const screenWidth = window.innerWidth;
 
     const quotes = [
-        {text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequatur recusandae in nostrum, mollitia laborum ipsa aliquid pariatur vitae veritatis accusantium.", id: 1},
-        {text: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Est optio itaque architecto velit, nobis repudiandae.", id: 2},
-        {text: "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Obcaecati, adipisci.", id: 3},
-        {text: "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quos velit natus numquam?", id: 4},
+        {text: "The teachers are very thoughtful and kind. They have a good understanding of cultural differences.", id: 1},
+        {text: "I had a very fun time during my class. The teachers are good listeners and give me opportunity to speak lots.", id: 2},
+        {text: "The tutors create a very friendly and easy-to-speak atmosphere. They will confirm the purpose of the class first, then proceed, and will answer your questions accurately.", id: 3},
+        {text: "It was very relaxing to speak in English. I learnt many expressions. The lessons were very helpful as well.", id: 4},
     ]
     
     return ( 
