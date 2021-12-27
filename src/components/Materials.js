b31ab8d45a489d8d5d1b9e96239a74bf2c75798e
Adam Bolton <adam_bolton90@hotmail.com>
2021-12-27 22:55:03 +0900

    added a responsive header to Materials component

--- src/components/Materials.js
+++ src/components/Materials.js
@@ -3,6 +3,7 @@ import MaterialsHero from "./MaterialsHero";
 const Materials = () => {
     return ( 
         <div className="materials">
+            <h1 className="materials-header">Lorem ipsum dolor sit amet consectetur adipisicing elit. Quidem numquam fuga, saepe iure perferendis corporis?</h1>
             <MaterialsHero />
         </div>
      );
