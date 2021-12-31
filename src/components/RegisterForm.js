c5904e87985b0ce29596246c3eaa8790e00c63c2
Adam Bolton <adam_bolton90@hotmail.com>
2022-01-01 00:46:41 +0900

    added styling to register form

--- src/components/RegisterForm.js
+++ src/components/RegisterForm.js
@@ -2,23 +2,27 @@ const RegisterForm = () => {
     return ( 
         <div>
             <form className="register-form">
-                <div>
+                <h2>Register Lorem ipsum dolor sit, amet consectetur adipisicing elit. Sit, architecto!</h2>
+                <div className="register-form-names">
                     <label>First Name</label>
                     <input type="text" required/>
-                    
                     <label>Last Name</label>
                     <input type="text" required/>
                 </div>
-
                 <br />    
                 <label>Username</label>
                 <input type="text" required/>
+                <br />
                 <label>Email</label>
                 <input type="text" required/>
+                <br />
                 <label>Password</label>
-                <input type="text" required/>
+                <input type="password" required/>
+                <br />
                 <label>Confirm Password</label>
-                <input type="text" required/>   
+                <input type="password" required/>
+                <br /><br />
+                <button>Submit</button>
             </form>
         </div>
         
