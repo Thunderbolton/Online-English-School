089dcde0ba6acb873a517fa4ba4b39ada0161346
Adam Bolton <adam_bolton90@hotmail.com>
2022-02-20 19:26:32 +0900

    added external links to projects

--- src/components/MaterialsHero.js
+++ src/components/MaterialsHero.js
@@ -22,20 +22,20 @@ const MaterialsHero = () => {
             
             <div id="conversation-info" className="materials-hero-info" onMouseEnter={conversationContrastChange} onMouseLeave={() => setConversationContrast("materials-hero-img")}>
                 <p>Ease into lessons naturally using this material to engage in daily conversation. Select from a variety of 100 commonly talked-about topics.</p>
-                <button id="conversation-btn" className="materials-hero-info-btn">Jump into daily conversation</button>
+                <a href="https://thunderbolton.github.io/Conversation-app/" target="_blank"><button id="conversation-btn" className="materials-hero-info-btn">Jump into daily conversation</button></a>
             </div>
             <img id="conversation-img" src={conversation} alt="a collection of conversation topics" className={conversationContrast}></img>
 
             <div id="news-info" className="materials-hero-info" onMouseEnter={newsContrastChange} onMouseLeave={() => setNewsContrast("materials-hero-img")}>
                 <p>Explore a range of news categories and read articles from a selection of countries. Enjoy discussion on the latest stories.</p>
-                <button id="news-btn" className="materials-hero-info-btn">Read the latest news</button>
+                <a href="https://readthelatest.herokuapp.com/" target="_blank"><button id="news-btn" className="materials-hero-info-btn">Read the latest news</button></a>
             </div>
             <img id="news-img" src={news} alt="a page with news articles" className={newsContrast}/>
 
             {/* For smaller screen sizes */}
             <div className="materials-hero-info-btn-sm">
-                <button id="conversation-btn-sm" onMouseEnter={conversationContrastChange} onMouseLeave={() => setConversationContrast("materials-hero-img")}>Jump into daily conversation</button>
-                <button id="news-btn-sm" onMouseEnter={newsContrastChange} onMouseLeave={() => setNewsContrast("materials-hero-img")}>Read the latest news</button>
+                <a href="https://thunderbolton.github.io/Conversation-app/" target="_blank"><button id="conversation-btn-sm" onMouseEnter={conversationContrastChange} onMouseLeave={() => setConversationContrast("materials-hero-img")}>Jump into daily conversation</button></a>
+                <a href="https://readthelatest.herokuapp.com/" target="_blank"><button id="news-btn-sm" onMouseEnter={newsContrastChange} onMouseLeave={() => setNewsContrast("materials-hero-img")}>Read the latest news</button></a>
             </div>
         </div>
      );
